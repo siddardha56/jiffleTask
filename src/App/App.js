dceb2f9f801e2dd3c2f495dc49ae49892ef7c3ab
siddardha.raju <siddardha.raju@fptechscience.com>
2018-12-08 15:43:15 +0530

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.scss';
+
+const App = () => (
+  <div className={styles.app}>
+    <div className={styles.content}>FPTS App Skeleton</div>
+  </div>
+);
+
+export default App;
